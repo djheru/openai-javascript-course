701719e6723ffa7e7c71ddc38061583346ddf492
Philip Damra <pdamra@PDamraMBP.local>
2023-10-09 22:08:11 -0700

    Plan and execute agent

--- playground/quickstart.mjs
+++ playground/quickstart.mjs
@@ -19,48 +19,48 @@ console.log("SERPAPI_API_KEY", SERPAPI_API_KEY);
 console.log("Welcome to the LangChain Quickstart Module!");
 
 // Format the prompt template
-const template = `
-You are a social media director with 20 years of experience.
-Please give me some ideas for content I should write about regarding {topic}? 
-The content is for {socialPlatform}. Translate to {language}.
-`;
-const inputVariables = ["topic", "socialPlatform", "language"];
-const prompt = new PromptTemplate({ template, inputVariables });
-const inputParams = {
-  topic: "artificial intelligence",
-  socialPlatform: "Twitter",
-  language: "English",
-};
-const formattedPromptTemplate = await prompt.format(inputParams);
-
-console.log(formattedPromptTemplate);
+// const template = `
 // You are a social media director with 20 years of experience.
-// Please give me some ideas for content I should write about regarding artificial intelligence?
-// The content is for Twitter. Translate to French.
+// Please give me some ideas for content I should write about regarding {topic}?
+// The content is for {socialPlatform}. Translate to {language}.
+// `;
+// const inputVariables = ["topic", "socialPlatform", "language"];
+// const prompt = new PromptTemplate({ template, inputVariables });
+// const inputParams = {
+//   topic: "artificial intelligence",
+//   socialPlatform: "Twitter",
+//   language: "English",
+// };
+// const formattedPromptTemplate = await prompt.format(inputParams);
 
-// Initialize the OpenAI API
-const llm = new OpenAI({ temperature: 0.9 });
+// console.log(formattedPromptTemplate);
+// // You are a social media director with 20 years of experience.
+// // Please give me some ideas for content I should write about regarding artificial intelligence?
+// // The content is for Twitter. Translate to French.
 
-// Create the chain
-const chain = new LLMChain({ llm, prompt });
+// // Initialize the OpenAI API
+// const llm = new OpenAI({ temperature: 0.9 });
 
-// Execute the chain
-const resChain = await chain.call(inputParams);
+// // Create the chain
+// const chain = new LLMChain({ llm, prompt });
 
-console.log(resChain);
+// // Execute the chain
+// const resChain = await chain.call(inputParams);
 
-// Chains vs Agents
-// Chain -> pre-defined prompt templates, step by step
-// Agent -> dynamically generated prompt template, steps decided by agent
+// console.log(resChain);
 
-// Initialize the Agent
-const agentModel = new OpenAI({
-  temperature: 0,
-  // https://platform.openai.com/docs/models
-  modelName: "text-davinci-003",
-}); // temperature 0 means deterministic
+// // Chains vs Agents
+// // Chain -> pre-defined prompt templates, step by step
+// // Agent -> dynamically generated prompt template, steps decided by agent
 
-// Create agent tools
+// // Initialize the Agent
+// const agentModel = new OpenAI({
+//   temperature: 0,
+//   // https://platform.openai.com/docs/models
+//   modelName: "text-davinci-003",
+// }); // temperature 0 means deterministic
+
+// // Create agent tools
 const tools = [
   new SerpAPI(process.env.SERPAPI_API_KEY, {
     location: "Phoenix,Arizona,United States",
@@ -70,20 +70,47 @@ const tools = [
   new Calculator(),
 ];
 
-const executor = await initializeAgentExecutorWithOptions(tools, agentModel, {
-  agentType: "zero-shot-react-description",
-  verbose: true,
-  maxIterations: 10,
-});
+// const executor = await initializeAgentExecutorWithOptions(tools, agentModel, {
+//   agentType: "zero-shot-react-description",
+//   verbose: true,
+//   maxIterations: 10,
+// });
 
-// ChatGPT does not know what the answer to this question is, due to knowledge cutoff
-// The agent will use the serpapi tool to get the answer
-const input = "What is langchain?";
+// // ChatGPT does not know what the answer to this question is, due to knowledge cutoff
+// // The agent will use the serpapi tool to get the answer
+// const input = "What is langchain?";
 
-const res = await executor.call({ input });
+// const res = await executor.call({ input });
 
-console.log(res);
+// console.log(res);
 
 /**
+ *
  * Plan and action agent
+ *
+ * Works with a chat model
+ */
+const llm = new ChatOpenAI({
+  temperature: 0,
+  modelName: "gpt-3.5-turbo",
+  verbose: true,
+});
+
+const executor = PlanAndExecuteAgentExecutor.fromLLMAndTools({
+  llm,
+  tools,
+});
+
+const result = await executor.call({
+  // We don't tell it how to do it, we just tell it what to do
+  input:
+    "Who is the current president of the United States? What is their age to the 2nd power?",
+});
+
+console.log(result);
+
+/**
+ *
+ * Memory
+ *
  */
